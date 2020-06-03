61fb86b136128b5574f3f40a31a698f68e35804f
Suman Barick <codotronix@gmail.com>
2020-06-04 00:13:04 +0530

    removed unused dependencies

--- src/components/page-modules/trackpad/Trackpad.js
+++ src/components/page-modules/trackpad/Trackpad.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import { makeStyles } from '@material-ui/core'
 import ArrowUpwardIcon from '@material-ui/icons/ArrowUpward'
 import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward'
