8d8e6cd33e3891f81aec47176524460ba5e0f8f7
Suman Barick <codotronix@gmail.com>
2020-06-24 18:32:47 +0530

    Updates

---
+++ precache-manifest.9d9cebf9d126d49d7e7f99bd40360b65.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6fac7417db10167e8338c16205d11dd",
+    "url": "/pcr-mc/index.html"
+  },
+  {
+    "revision": "c0a31da0a4895eea2766",
+    "url": "/pcr-mc/static/css/main.42eb543d.chunk.css"
+  },
+  {
+    "revision": "27dfb248585f28eddedd",
+    "url": "/pcr-mc/static/js/2.13c0a92c.chunk.js"
+  },
+  {
+    "revision": "68ba00d5eb083746d913686923a3d904",
+    "url": "/pcr-mc/static/js/2.13c0a92c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c0a31da0a4895eea2766",
+    "url": "/pcr-mc/static/js/main.fae45def.chunk.js"
+  },
+  {
+    "revision": "413e05f0b42071bd391d",
+    "url": "/pcr-mc/static/js/runtime-main.ad1f65b7.js"
+  }
+]);
